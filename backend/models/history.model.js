ee3c993c720af39aacedf4a57c99c90f729c18ae
Kumar Ayaman <142806861+kr-Ayaman@users.noreply.github.com>
2025-06-04 20:06:42 +0530

    model

--- backend/models/history.model.js
+++ backend/models/history.model.js
@@ -1,34 +1,34 @@
 import mongoose from "mongoose";
 
 const historySchema = new mongoose.Schema({
-group: {
-type: mongoose.Schema.Types.ObjectId,
-ref: "Group",
-required: true,
-},
-event: {
-type: mongoose.Schema.Types.ObjectId,
-ref: "Event",
-required: true,
-},
-watchedMovie: {
-type: mongoose.Schema.Types.ObjectId,
-ref: "Movie",
-required: true,
-},
-watchedOn: {
-type: Date,
-required: true,
-},
-attendees: [
-{
-type: mongoose.Schema.Types.ObjectId,
-ref: "User",
-},
-],
-notes: {
-type: String,
-}
+    group: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Group",
+        required: true,
+    },
+    event: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Event",
+        required: true,
+    },
+    watchedMovie: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Movie",
+        required: true,
+    },
+    watchedOn: {
+        type: Date,
+        required: true,
+    },
+    attendees: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+    notes: {
+        type: String,
+    }
 }, { timestamps: true });
 
 const History = mongoose.model("History", historySchema);
