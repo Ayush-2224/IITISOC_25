3528b0f9484d468b8021dad935c91d51918c9b5a
Kumar Ayaman <142806861+kr-Ayaman@users.noreply.github.com>
2025-06-12 01:39:32 +0530

    event backend

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import connectDB from "./config/mongodb.js";
 import userRouter from "./routes/userRoute.js";
 import passport from './config/googleAuth.js';
 import session from "express-session";
-
+import eventsRoute from "./routes/events.route.js";
 
 // express app
 const app =express()
@@ -40,7 +40,7 @@ app.use("/api/user",userRouter);
 app.get("/",(req,res)=>{
     res.send("api working")
 })
-
+app.use("/api/events",eventsRoute);
 // Error handling middleware
 app.use((error, req, res, next) => {
     if (error.code === 'LIMIT_UNEXPECTED_FILE') {
